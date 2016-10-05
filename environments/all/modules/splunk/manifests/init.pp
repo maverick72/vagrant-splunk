b56934a0617858a460a29ba8ac91f068396eed5c
Eric Berthiaume <eric.berthiaume@gmail.com>
2016-10-05 16:30:37 +0200

    syntax reformating

---
+++ environments/all/modules/splunk/manifests/init.pp
@@ -0,0 +1,51 @@
+# Splunk main module
+class splunk {
+  # Make sure the needed dirs exist
+  file { ['/opt/splunk', '/opt/splunk/etc','/opt/splunk/etc/apps']:
+    ensure  => directory,
+  }
+
+  # Install splunk
+  package { 'splunk':
+    ensure   => installed,
+    source   => "file:///media/rpms/$::{splunk_rpm}",
+    provider => 'rpm'
+  }
+
+  # Install the splunk init script and accept the license
+  exec { '/etc/init.d/splunk' :
+    command   => '/opt/splunk/bin/splunk enable boot-start --accept-license --no-prompt --answer-yes',
+    subscribe => Package['splunk']
+  }
+
+  # Make sure the first time login message does not show up
+  file { '/opt/splunk/etc/.ui_login':
+    ensure  => 'file',
+    require => Package['splunk']
+  }
+
+  # Start the service
+  service { 'splunk':
+    ensure  => 'running',
+    enable  => true,
+    require => Exec[ '/etc/init.d/splunk' ]
+  }
+
+  # Set the admin passoword to admin
+  exec { 'splunkpasswordchange':
+    command => '/opt/splunk/bin/splunk edit user admin -password admin -role admin -auth admin:changeme || exit 0',
+    require => Service['splunk']
+  }
+
+  # Ensure directory exists
+  file { '/opt/splunk/etc/system/local':
+    ensure => directory,
+  }
+  # Copy the custom web.conf
+  file { '/opt/splunk/etc/system/local/web.conf':
+    source  => 'puppet:///modules/splunk/web.conf',
+    require => File['/opt/splunk/etc/system/local'],
+    notify  => Service['splunk']
+  }
+
+}

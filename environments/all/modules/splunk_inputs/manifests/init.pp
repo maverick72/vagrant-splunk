5e120971b06b17c012a66af528db2a862c982abd
Eric Berthiaume <eric.berthiaume@gmail.com>
2016-10-06 15:54:01 +0200

    syntax

---
+++ environments/all/modules/splunk_inputs/manifests/init.pp
@@ -0,0 +1,8 @@
+# splunk init manifest for inputs template generation
+class splunk_inputs {
+  file { '/opt/splunk/etc/system/local/inputs.conf':
+    require => Package['splunk'],
+    content => template('splunk-inputs/inputs.conf.erb'),
+    notify  => Service['splunk']
+  }
+}

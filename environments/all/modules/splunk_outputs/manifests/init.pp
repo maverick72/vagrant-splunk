42a56b5e976a29f4f15d7b45edcca0e60511d3db
Eric Berthiaume <eric.berthiaume@gmail.com>
2016-10-07 17:15:04 +0200

    change for double quotes

--- environments/all/modules/splunk_outputs/manifests/init.pp
+++ environments/all/modules/splunk_outputs/manifests/init.pp
@@ -1,6 +1,6 @@
 # splunk outputs manifest
 class splunk_outputs ($type='splunkforwarder'){
-  file { '/opt/$type/etc/system/local/outputs.conf':
+  file { "/opt/${type}/etc/system/local/outputs.conf":
     require => Package[$type],
     content => template('splunk_outputs/outputs.conf.erb'),
     notify  => Service['splunk']

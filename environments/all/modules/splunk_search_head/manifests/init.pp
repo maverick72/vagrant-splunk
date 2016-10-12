dae8bbfdea46fd02fd4b22259a9466145f75a7d5
Eric Berthiaume <eric.berthiaume@gmail.com>
2016-10-12 12:07:14 +0200

    module syntax

---
+++ environments/all/modules/splunk_search_head/manifests/init.pp
@@ -0,0 +1,12 @@
+# splunk search head manifest
+class splunk_search_head {
+  exec { 'enable-search-head':
+    command => "/opt/splunk/bin/splunk edit cluster-config -mode \
+searchhead -master_uri https://$::{master_ip}:8089 -auth admin:admin || exit 0",
+    require => [Package['splunk'],Service['splunk'],Exec['splunkpasswordchange']],
+  }
+  exec { 'enable-search-head-splunk-restart':
+    command => '/opt/splunk/bin/splunk restart || exit 0',
+    require => Exec['enable-search-head']
+  }
+}

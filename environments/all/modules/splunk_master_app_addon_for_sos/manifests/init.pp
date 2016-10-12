c7d090b969502dd0409e6b1621074cb16cdd5e32
Eric Berthiaume <eric.berthiaume@gmail.com>
2016-10-12 11:55:30 +0200

    module syntax

---
+++ environments/all/modules/splunk_master_app_addon_for_sos/manifests/init.pp
@@ -0,0 +1,12 @@
+# splunk app addon for sos manifest
+class splunk_master_app_addon_for_sos {
+  file { '/opt/splunk/etc/master-apps/TA-sos/local/indexes.conf':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    recurse => true,
+    source  => 'puppet:///modules/splunk-master-app-addon-for-sos/TA-sos/local/indexes.conf',
+    notify  => Service['splunk'],
+    require => File['/opt/splunk/etc/master-apps/TA-sos/local']
+  }
+}

5f0b66331ee0ed8f11f2faf0446b46ff0544678d
Eric Berthiaume <eric.berthiaume@gmail.com>
2016-10-12 11:51:37 +0200

    module syntax

---
+++ environments/all/modules/splunk_master_app_addon_for_nix/manifests/init.pp
@@ -0,0 +1,9 @@
+# splunk manifest for app addon for nix
+class splunk_master_app_addon_for_nix {
+  file { '/opt/splunk/etc/master-apps/Splunk_TA_nix/local/indexes.conf':
+    ensure  => 'file',
+    source  => 'puppet:///modules/splunk-master-app-addon-for-nix/Splunk_TA_nix/local/indexes.conf',
+    notify  => Service['splunk'],
+    require => File['/opt/splunk/etc/master-apps/Splunk_TA_nix/local/']
+  }
+}

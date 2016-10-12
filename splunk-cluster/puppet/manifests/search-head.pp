c9d937ea2b9df42bc044b59444fa1ed49d0963e9
Eric Berthiaume <eric.berthiaume@gmail.com>
2016-10-12 12:13:59 +0200

    class syntax

--- splunk-cluster/puppet/manifests/search-head.pp
+++ splunk-cluster/puppet/manifests/search-head.pp
@@ -4,6 +4,6 @@ include splunk_search_head
 include splunk_app_for_nix
 include splunk_app_sos
 include splunk_app_sideview_utils
-class {'splunk-outputs':
+class {'splunk_outputs':
   type => 'splunk'
 }

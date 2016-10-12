6b7e578cace859b8403ce772e6d5e91fb8fa2e96
Eric Berthiaume <eric.berthiaume@gmail.com>
2016-10-12 11:45:32 +0200

    syntax change

---
+++ environments/all/modules/splunk_master/manifests/init.pp
@@ -0,0 +1,13 @@
+# splunk master manifest
+class splunk_master ($rep_factor=3,$search_factor=2) {
+  exec { 'enable-master':
+    command => "/opt/splunk/bin/splunk edit cluster-config \
+-mode master -replication_factor ${rep_factor} \
+-search_factor ${search_factor} -auth admin:admin",
+    require => [Package['splunk'],Service['splunk'],Exec['splunkpasswordchange']],
+  }
+  exec { 'enable-master-splunk-restart':
+    command => '/opt/splunk/bin/splunk restart || exit 0',
+    require => Exec['enable-master']
+  }
+}
